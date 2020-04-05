06d5e848de86a6b2804e83f5f86d0bb0774cc685
Med Elboudali <med.sneaky@gmail.com>
2020-04-05 01:33:41 +0000

    Install Materialize

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Logo from "./Assets/Images/logo.webp";
 
 function App() {
   return (
-    <div className="App">
-      <h1>My React Boilerplate</h1>
+    <div className="App container">
+      <div className="logo">
+        <img src={Logo} alt="Logo" />
+        <h1>IT Logger</h1>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css/dist/js/materialize.min.js";
 import "./Assets/Style/App.css";
 import App from "./App";
 
