2298dd14f9ad6686b763ae269b46623b9225edb4
MedEBoudali <med.sneaky@gmail.com>
2020-04-08 17:46:46 +0100

    Blue Btn

--- src/Components/Logs/AddLogModal.jsx
+++ src/Components/Logs/AddLogModal.jsx
@@ -68,7 +68,7 @@ const AddLogModal = () => {
         <a
           href="#!"
           onClick={onSubmit}
-          className="modal-close waves-effect waves-green btn-flat"
+          className="modal-close waves-effect waves-green btn blue"
         >
           Enter
         </a>
