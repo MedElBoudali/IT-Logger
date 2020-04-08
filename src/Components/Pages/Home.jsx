fe6b9d71686094f086513cf418f96898c8ca84b5
MedElBoudali <med.sneaky@gmail.com>
2020-04-08 01:19:52 +0100

    Add Fmoating Btn

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import SearchBar from "../Layouts/SearchBar";
 import LogsContainer from "../Logs/LogsContainer";
-
+import AddBtn from "../Layouts/AddBtn";
 
 const Home = () => {
   return (
     <div className="App container">
       <SearchBar />
+      <AddBtn />
       <LogsContainer />
     </div>
   );
