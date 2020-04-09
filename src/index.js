effcc61cc94593e3c7e691bcd20c49f1402c15a8
MedEBoudali <med.sneaky@gmail.com>
2020-04-09 22:59:56 +0100

    Add Store & Reducers

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import Store from "./Store";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider Store={Store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
