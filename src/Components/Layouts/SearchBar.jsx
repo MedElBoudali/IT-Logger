5773aead36f0d3788effdede6b81e28015106b83
MedEBoudali <med.sneaky@gmail.com>
2020-04-11 23:53:02 +0100

    Trying to fix Search Action

--- src/Components/Layouts/SearchBar.jsx
+++ src/Components/Layouts/SearchBar.jsx
@@ -1,12 +1,20 @@
 import React from "react";
+import { connect } from "react-redux";
+import { searchLogs } from "../../Actions/LogActions";
+import PropTypes from "prop-types";
 
-const SearchBar = () => {
+const SearchBar = ({ searchLogs }) => {
+  const onChange = (e) => {
+    // e.preventDefault();
+    // searchLogs(e.target.value);
+    console.log(e)
+  };
   return (
     <nav className="purple darken-3">
       <div className="nav-wrapper">
         <form>
           <div className="input-field">
-            <input id="search" type="search" />
+            <input id="search" type="search" placeholder="Search Logs ..." onChange={onChange} />
             <label className="label-icon" htmlFor="search">
               <i className="material-icons">search</i>
             </label>
@@ -18,4 +26,8 @@ const SearchBar = () => {
   );
 };
 
-export default SearchBar;
+SearchBar.prototype = {
+  searchLogs: PropTypes.func.isRequired,
+};
+
+export default connect(null, { searchLogs })(SearchBar);
