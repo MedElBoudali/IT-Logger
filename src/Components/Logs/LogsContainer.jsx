815322a2b2b09f3be6215586136e79af901ebfe9
MedEBoudali <med.sneaky@gmail.com>
2020-04-14 01:31:32 +0100

    :sparkles: Fixing & Optimizing the app

--- src/Components/Logs/LogsContainer.jsx
+++ src/Components/Logs/LogsContainer.jsx
@@ -1,32 +1,31 @@
-import React, { useEffect } from "react";
-import { connect } from "react-redux";
-import { getLogs } from "../../Actions/LogActions";
-import Logo from "../../Assets/Images/logo.webp";
-import LogItem from "./LogItem";
-import Preloader from "../Layouts/Preloader";
-import PropTypes from "prop-types";
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { getLogs } from '../../Actions/LogActions';
+import Logo from '../../Assets/Images/logo.webp';
+import LogItem from './LogItem';
+import Preloader from '../Layouts/Preloader';
+import PropTypes from 'prop-types';
 
 const LogsContainer = ({ log: { logs, loading }, getLogs }) => {
   useEffect(() => {
     getLogs();
-    // eslint-disable-next-line
-  }, []);
+  }, [getLogs]);
 
   if (loading || logs === null) {
     return <Preloader />;
   }
 
   return (
-    <ul className="collection with-header">
-      <li className="collection-header">
-        <h4 className="header center">
-          <img className="logo" src={Logo} alt="Logo" /> System Logs
+    <ul className='collection with-header'>
+      <li className='collection-header'>
+        <h4 className='header center'>
+          <img className='logo' src={Logo} alt='Logo' /> System Logs
         </h4>
       </li>
       {!loading && logs.length === 0 ? (
-        <p className="center">No Logs To Show !</p>
+        <p className='center'>No Logs To Show !</p>
       ) : (
-        logs.map((log) => <LogItem log={log} key={log.id} />)
+        logs.map(log => <LogItem log={log} key={log.id} />)
       )}
     </ul>
   );
@@ -34,9 +33,9 @@ const LogsContainer = ({ log: { logs, loading }, getLogs }) => {
 
 LogsContainer.prototype = {
   log: PropTypes.object.isRequired,
-  getLogs: PropTypes.func.isRequired,
+  getLogs: PropTypes.func.isRequired
 };
 
-const mapStateToProps = (state) => ({ log: state.log });
+const mapStateToProps = state => ({ log: state.log });
 
 export default connect(mapStateToProps, { getLogs })(LogsContainer);
--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,16 +1,15 @@
-import React from "react";
-import SearchBar from "../Layouts/SearchBar";
-import LogsContainer from "../Logs/LogsContainer";
-import AddBtn from "../Layouts/AddBtn";
-import AddModalLog from "../Logs/AddLogModal";
-import EditModalLog from "../Logs/EditLogModal";
-import AddTechModal from "../Techs/AddTechModal";
-import TechListModal from "../Techs/TechListModal";
+import React from 'react';
+import SearchBar from '../Layouts/SearchBar';
+import LogsContainer from '../Logs/LogsContainer';
+import AddBtn from '../Layouts/AddBtn';
+import AddModalLog from '../Logs/AddLogModal';
+import EditModalLog from '../Logs/EditLogModal';
+import AddTechModal from '../Techs/AddTechModal';
+import TechListModal from '../Techs/TechListModal';
 
 const Home = () => {
-
   return (
-    <div className="App container">
+    <div className='App container'>
       <SearchBar />
       <AddBtn />
       <AddModalLog />
